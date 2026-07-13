69523d3fbcd7bc4a265e63ef96ad96e20f52a0e5
Vikrant Mainwal <vikrantmainwal1301@gmail.com>
2026-07-13 14:41:35 +0530

    minor

--- features/checkout/lib/constants.ts
+++ features/checkout/lib/constants.ts
@@ -1,3 +1,3 @@
 // Free shipping above this subtotal (after coupon discount), flat fee below it.
 export const FREE_SHIPPING_THRESHOLD = 999;
-export const FLAT_SHIPPING_FEE = 99;
+export const FLAT_SHIPPING_FEE = 199;
